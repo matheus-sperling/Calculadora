d7ff64f3bcc7742add8a50e39f52c614b2c7795e
Matheus <jeferson.sperling@gmail.com>
2024-01-14 01:14:42 -0400

    Calculadora a ser concluída!

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Olá, esse código aqui é a minha calculadora em java!");
+        System.out.println("Por favor, insira seu número x: ");
+        double x = input.nextDouble();
+        System.out.println("Agora insira seu número y: ");
+        double y  = input.nextDouble();
+        System.out.println("Agora escolha a operação desejada: \n 1 - Multiplicação, 2 - Divisão, 3 - Soma, 4 - Subtração");
+        // Finalizarei mais tarde.
+    }
+}
